fe4bc5b7a63d8c1ca77ecc27460f6646ac797583
Wacu_Mutahi <28296097+Wacu@users.noreply.github.com>
2020-09-05 21:59:05 +0300

    If else control structure

---
+++ The JavaScript Console Challenge/js.js
@@ -0,0 +1,20 @@
+
+alert("Welcome to this page!");
+
+var Answer=prompt("What is 2 + 2?");
+	if(Answer==="4") {
+		
+		document.write("Correct answer");
+	else {
+		document.write("You are wrong");
+		prompt("Please try again");	
+	}	
+		
+//var Name=prompt("Whats your name?");
+//var color=prompt("Whats your favorite color?");
+//var Age=prompt("Whats your age?");
+
+//var yearsalive = parseInt(Age) * 365
+
+//document.write("Hello " + Name + " you have lived " + yearsalive + " days ");
+
