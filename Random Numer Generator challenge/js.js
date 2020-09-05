12d41854871a83d5f47b6634c8c2d5a53d295f67
Wacu_Mutahi <28296097+Wacu@users.noreply.github.com>
2020-09-05 21:56:38 +0300

    Lets generate a random number

---
+++ Random Numer Generator challenge/js.js
@@ -0,0 +1,6 @@
+
+var Usernumber= parseInt(prompt("Enter your first number"));
+//Usernumber = parseInt(Usernumber);
+var randnumber=Math.floor(Math.random()*Usernumber)+1;
+
+alert("Your random number is "+ randnumber);
